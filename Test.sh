36e1070a483b2ed03b932adf557797b28dd8756b
breakwa2333 <48397771+breakwa2333@users.noreply.github.com>
2019-07-31 22:01:28 +0800

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,5 +1,5 @@
 {
-  Release_URL=$(curl -L https://api.github.com/repos/v2ray/v2ray-core/tags | grep zipball_url | cut -d$'\n' -f1 | cut -d'"' -f4)
+  Release_URL=$(curl -L https://api.github.com/repos/EtherDream/jsproxy/tags | grep zipball_url | cut -d$'\n' -f1 | cut -d'"' -f4)
   wget -o jsproxy.tar.gz $Release_URL
   tar -xzvf jsproxy.tar.gz
   rm -f jsproxy.tar.gz
