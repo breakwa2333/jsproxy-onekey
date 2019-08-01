3c02c8fc86e61943eed0725a357ee89432e62d52
breakwa2333 <48397771+breakwa2333@users.noreply.github.com>
2019-08-01 14:19:51 +0800

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -2,14 +2,14 @@
   Release_URL=$(curl -L https://api.github.com/repos/EtherDream/jsproxy/tags | grep zipball_url | cut -d$'\n' -f1 | cut -d'"' -f4)
   echo 11111
   curl -L -o jsproxy.tar.gz $Release_URL
-  tar -xzvf jsproxy.tar.gz
+  unzip jsproxy.tar.gz
   rm -f jsproxy.tar.gz
   echo 22222
-  curl -L -o jsproxy.tar.gz $Release_URL
-  tar -xf jsproxy.tar.gz
-  rm -f jsproxy.tar.gz
+  curl -L -o jsproxy.tar $Release_URL
+  unzip jsproxy.tar
+  rm -f jsproxy.tar
   echo 33333
-  curl -L -o jsproxy.tar.gz $Release_URL
-  tar -xfvw jsproxy.tar.gz
-  rm -f jsproxy.tar.gz
+  curl -L -o jsproxy.zip $Release_URL
+  unzip jsproxy.zip
+  rm -f jsproxy.zip
 }
